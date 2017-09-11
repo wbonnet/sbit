b1e283c26c3f511f432a8530b63153ccf269f9a0
William BONNET <wbonnet@theitmakers.com>
2017-09-11 08:33:03 +0200

    Inittial commit

---
+++ sbit/check_testsuite.py
@@ -0,0 +1,82 @@
+#
+# The contents of this file are subject to the Apache 2.0 license you may not
+# use this file except in compliance with the License.
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+#
+# Copyright 2017 DFT project (http://www.debianfirmwaretoolkit.org).
+# All rights reserved. Use is subject to license terms.
+#
+#
+# Contributors list :
+#
+#    William Bonnet     wllmbnnt@gmail.com, wbonnet@theitmakers.com
+#
+#
+
+
+""" This modules implements the functionnalities used to check consistency of a test suite.
+Consistency check will control the test file and look for inexistent test scripts, redundant IDs,
+malformed yaml fragment, etc.
+"""
+
+import logging
+import os
+import stat
+import tempfile
+import datetime
+from cli_command import CliCommand
+from model import Key
+
+#
+#    Class CheckTestSuite
+#
+class CheckTestSuite(CliCommand):
+  """This class implements methods needed to load a test suite from the configuration files,
+  and control its validity.
+
+  Results are display to output according to parameters from the command line. These paraeters
+  are stored in the configuration object by the CLI parser
+  """
+
+  # -------------------------------------------------------------------------
+  #
+  # __init__
+  #
+  # -------------------------------------------------------------------------
+  def __init__(self, cfg):
+    """Default constructor
+    """
+
+    # Initialize ancestor
+    CliCommand.__init__(self, cfg)
+
+
+
+  # -------------------------------------------------------------------------
+  #
+  # check_suite
+  #
+  # -------------------------------------------------------------------------
+  def check_suite(self):
+    """This method implement the business logic of checking a test suite.
+    Checking a test suite is :
+      - XXX
+    """
+
+    # Check that there is a firmware configuration file first
+    if self.cfg is None:
+      logging.critical("The configuration file object is not defined")
+      exit(1)
+
+    print("check_suite")
+
+    if self.cfg.library_path:
+      print(self.cfg.library_path)
+
+    if self.cfg.suite_path:
+      print(self.cfg.suite_path)
